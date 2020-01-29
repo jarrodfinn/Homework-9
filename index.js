bef9ab065b41a88a383a4b4f4546d79be22d2d23
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-01-28 22:37:51 -0500

    generating pdf

--- index.js
+++ index.js
@@ -1,66 +1,48 @@
-// The user will be prompted for a favorite color, which will be used as the background color for cards.
-// The PDF will be populated with the following:
-
-// const questions = [
-//     // what is your Github user name?
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-  .prompt({
+const generateHtml = require ("./generateHTML");
+const pdf = require('html-pdf');
+
+const questions = [
+  {
+    type: "prompt",
+    name: "githubname",
+    message: "Enter your GitHub username:"
+  },
+  {
+    type: "list",
+    name: "color",
     message: "What is your favorie color?",
-    // array question containing: 
-    // // Green
-    // // Blue
-    // // Pink
-    // // Red
-
-  })
-  .then(function ({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    axios.get(queryUrl).then(function (res) {
-      const repoNames = res.data.map(function (repo) {
-        return repo.name;
+    choices: ["green", "blue", "pink", "red"]
+  }
+];
+// function writeToFile(fileName, data) {
+//   // Profile image
+//   // User name
+//   // Links to the following:
+//   // User location via Google Maps
+//   // User GitHub profile
+//   // User blog
+//   // User bio
+//   // Number of public repositories
+//   // Number of followers
+//   // Number of GitHub stars
+//   // Number of users following
+// }
+function init() {
+  inquirer.prompt(questions).then(({ githubname, color }) => {
+    console.log(color);
+    const queryUrl = `https://api.github.com/users/${githubname}`;
+    axios.get(queryUrl).then(function(res) {
+      // console.log(res.data);
+      return generateHtml({color, ...res.data})
+    }).then(html => {
+      pdf.create(html).toStream(function(err, stream){
+        stream.pipe(fs.createWriteStream('./foo.pdf'));
       });
-
-
-
-      const repoNamesStr = repoNames.join("\n");
-      fs.writeFile("repos.txt", repoNamesStr, function (err) {
-        if (err) {
-          throw err;
-        }
-        console.log(`Saved ${repoNames.length} repos`);
-      });
-    });
+    })
   });
-    
+};
 
-function writeToFile(fileName, data) {
-
-
-  // Profile image
-// User name
-// Links to the following:
-
-// User location via Google Maps
-// User GitHub profile
-// User blog
-
-
-// User bio
-// Number of public repositories
-// Number of followers
-// Number of GitHub stars
-// Number of users following
-
-
-}
-
-function init() {}
-
-init()
+init();
